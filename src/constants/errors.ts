19f3275a090e73d1624881858c01a57c22e1c415
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-02-22 17:38:08 +0100

    feat(errors): add access denied case

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,3 +1,10 @@
+export class AccessDeniedError extends Error {
+  constructor(public cause: string) {
+    super("Access denied, you can't do this request.");
+    this.name = "AccessDeniedError";
+  }
+}
+
 export class BadCredentialsError extends Error {
   constructor(public cause: string) {
     super("Bad credentials, please check your access token and associated device identifier.");
