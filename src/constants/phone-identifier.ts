e880c0f4dd51232bc032c1622faa925cb5661281
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-02-22 16:31:14 +0100

    fix: constant phone identifier (to change later)

--- src/constants/phone-identifier.ts
+++ src/constants/phone-identifier.ts
@@ -1,3 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
-
-export const PHONE_IDENTIFIER = uuidv4();
+// NOTE: this is probably a bad security practice because auth is linked
+//       to this, just like a device-id; we might need to provide a way to
+//       make this dynamic instead of static in the future.
+export const PHONE_IDENTIFIER = "credit-agricole-mobile-api";
