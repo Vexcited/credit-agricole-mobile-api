3db1a19da55318d0a59efdd11b3f8871da6d051a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-02-24 18:33:36 +0100

    feat: add future transfers

--- examples/operations.ts
+++ examples/operations.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { join } from "node:path";
 
-import { getFutureOperationV1FutureCardOperations, getHomeV2FirstAccount, getOperationV2Operations } from "../src";
+import { getFutureOperationV1FutureCardOperations, getFutureOperationV1FutureTransfers, getHomeV2FirstAccount, getOperationV2Operations } from "../src";
 
 // Load the `.env` file configuration.
 config({ path: join(__dirname, ".env") });
@@ -17,14 +17,20 @@ void async function main () {
   const account = await getHomeV2FirstAccount(accessToken);
   const { operations } = await getOperationV2Operations(accessToken, account.contract_number);
   const { operations: futureOperations, preauthorized_operations: futurePreAuthOperations } = await getFutureOperationV1FutureCardOperations(accessToken, account.contract_number);
+  const { future_transfers: futureTransfers, total_amount: futureTransfersTotalAmount } = await getFutureOperationV1FutureTransfers(accessToken, account.contract_number);
 
   const allFutureOperations = [...futureOperations, ...futurePreAuthOperations];
   allFutureOperations.sort((a, b) => b.date_heure - a.date_heure);
 
   const currentBalance = account.balance.amount;
-  const futureBalance = allFutureOperations.reduce((acc, operation) => acc + operation.montant_en_euro.montant, currentBalance);
+  const futureBalance = allFutureOperations.reduce((acc, operation) => acc + operation.montant_en_euro.montant, currentBalance) - futureTransfersTotalAmount;
 
-  console.log(`Currently at ${currentBalance}, but will be at ${futureBalance.toFixed(2)} after all future operations.`);
+  console.log(`Currently at ${currentBalance.toFixed(2)}, but will be at ${futureBalance.toFixed(2)} after all future operations and transfers.`);
+
+  console.log("\nFUTURE TRANSFERS\n-----------------");
+  for (const transfer of futureTransfers) {
+    console.log(transfer.amount.toFixed(2), "EUR", "@", transfer.creditor_name);
+  }
 
   console.log("\nFUTURE OPERATIONS\n-----------------");
   for (const operation of allFutureOperations) {
